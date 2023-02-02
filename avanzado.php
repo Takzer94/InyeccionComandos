29567f680cd32e6a32af49488b4c4d8c9e4d9f79
Emmanuel Parra <42576422+Takzer94@users.noreply.github.com>
2023-02-02 15:28:19 -0600

    Add files via upload

---
+++ avanzado.php
@@ -0,0 +1,79 @@
+<?php
+
+if( isset( $_POST[ 'Submit' ]  ) ) {
+    // Input
+    $target = $_REQUEST[ 'ip' ];
+
+    // Blacklist
+    $substitutions = array(
+        '&'  => '',
+        ';'  => '',
+        '| ' => '',
+        '_'  => '',
+        '$'  => '',
+        '('  => '',
+        ')'  => '',
+        '`'  => '',
+        'cat' => '',
+        'pwd' => '',
+        'ls' => '',
+        'whereis' => '',
+        'file' => '',
+        'sudo' => '',
+        'cp' => '',
+        'rm' => '',
+        'touch' => '',
+        'find' => '',
+        'useradd' => '',
+        'su' => '',
+        'ps' => '',
+        'version' => '',
+        'ver' => '',
+    );
+
+    $target = str_replace( array_keys( $substitutions ), $substitutions, $target );
+
+    // Ejecutar Comando Ping.
+    if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
+        // Windows
+        $cmd = shell_exec( 'ping  ' . $target );
+    }
+    else {
+        // *nix
+        $cmd = shell_exec( 'ping  -c 4 ' . $target );
+    }
+
+    // Respuesta
+    echo "<pre>{$cmd}</pre>";
+}
+
+?> 
+<html>
+<body>
+	<form action="" method="post">
+	Ingrese una dirección IP: <input type="text" name="ip"><br>
+	<input type="submit" name="Submit" value="Submit">
+</form>
+<!--
+// Eliminar
+    $ListaNegra = array(
+        'cat' => '',
+        'pwd' => '',
+        'ls' => '',
+        'whereis' => '',
+        'file' => '',
+        'sudo' => '',
+        'cp' => '',
+        'rm' => '',
+        'touch' => '',
+        'find' => '',
+        'useradd' => '',
+        'su' => '',
+        'ps' => '',
+        'version' => '',
+        'ver' => '',
+    );
+
+-->
+</body>
+</html>
---
+++ intermedio.php
@@ -0,0 +1,60 @@
+<?php
+
+if( isset( $_POST[ 'Submit' ]  ) ) {
+    // Input
+    $target = trim($_REQUEST[ 'ip' ]);
+
+    // Blacklist
+    $substitutions = array(
+        '&'  => '',
+        ';'  => '',
+        '| ' => '',
+        '-'  => '',
+        '$'  => '',
+        '('  => '',
+        ')'  => '',
+        '`'  => '',
+        '||' => '',
+    );
+
+    $target = str_replace( array_keys( $substitutions ), $substitutions, $target );
+
+    // Ejecutar Comando Ping.
+    if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
+        // Windows
+        $cmd = shell_exec( 'ping  ' . $target );
+    }
+    else {
+        // *nix
+        $cmd = shell_exec( 'ping  -c 4 ' . $target );
+    }
+
+    // Respuesta
+    echo "<pre>{$cmd}</pre>";
+}
+
+?> 
+<html>
+<body>
+	<form action="" method="post">
+	Ingrese una dirección IP: <input type="text" name="ip"><br>
+	<input type="submit" name="Submit" value="Submit">
+</form>
+
+<!--
+// Eliminar
+    $ListaNegra = array(
+        '&'  => '',
+        ';'  => '',
+        '| ' => '',
+        '-'  => '',
+        '$'  => '',
+        '('  => '',
+        ')'  => '',
+        '`'  => '',
+        '||' => '',
+    );
+
+-->
+</body>
+</html>
